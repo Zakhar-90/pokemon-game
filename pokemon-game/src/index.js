5bd60f37f97740bd9e3c8a3454b6e7ca62fd8331
Zakhar-90 <vladimirzax@mail.ru>
2021-03-11 00:09:30 +0300

    Second commit

--- pokemon-game/src/index.js
+++ pokemon-game/src/index.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import HeaderBlock from './components/HeaderBlock';
+
+import './index.css';
+
+const AppList = () => {
+  const items = ['Item1', 'Item2', 'Item3', 'Item4'];
+  return (
+    <ul>
+      {
+        items.map(item => <li>{ item }</li>)
+      }
+      <li>{ items[0] }</li>
+      <li>{ items[1] }</li>
+    </ul>
+  );
+}
+
+const AppHeader = () => {
+  return (
+      <h1 
+        className="header"
+      >
+        Hello World!
+      </h1>
+    );
+}
+
+const AppInput = () => {
+  const placeholder = 'Type text...';
+
+  return (
+    <label>
+      <input placeholder={placeholder} />
+    </label>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+      <HeaderBlock />
+      <AppHeader />
+      <AppInput />
+      <AppList />
+      <AppHeader />
+      <AppList />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
